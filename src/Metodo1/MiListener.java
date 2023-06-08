8505f3f6ff6de6df5868fb63d216148c35894ffd
josediaz177 <josediazj8287@gmail.com>
2023-06-08 16:35:49 -0600

    Se agrega el proyecto

---
+++ src/Metodo1/MiListener.java
@@ -0,0 +1,15 @@
+package Metodo1;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MiListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+    
+}
+
